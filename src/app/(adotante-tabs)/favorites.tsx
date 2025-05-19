24a5c7d26685ce2e51d799d4dbfc3df784533683
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-05-19 17:16:50 +0000

    feat: criar função de desfavoritar na tela de favoritos

--- src/app/(adotante-tabs)/favorites.tsx
+++ src/app/(adotante-tabs)/favorites.tsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 import { colors } from "../../styles/colors";
 
 export default function FavoritesScreen() {
-  const favoritos = [
+  const [favoritos, setFavoritos] = useState([
     {
       id: 1,
       nome: "Bolt",
@@ -29,7 +30,11 @@ export default function FavoritesScreen() {
         uri: "https://images.unsplash.com/photo-1573865526739-10659fec78a5?q=80&w=2030",
       },
     },
-  ];
+  ]);
+
+  const toggleFavorito = (id: number) => {
+    setFavoritos((prev) => prev.filter((pet) => pet.id !== id));
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-white px-4 py-4 gap-6">
@@ -37,21 +42,34 @@ export default function FavoritesScreen() {
         data={favoritos}
         keyExtractor={(item) => item.id.toString()}
         showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          <Text className="text-center text-gray-500 mt-10">
+            Nenhum pet favoritado.
+          </Text>
+        }
         renderItem={({ item: pet }) => (
-          <View className="bg-gray-50 border border-gray-200 rounded-xl mb-4 p-4 flex-row items-center gap-4">
+          <View className="bg-gray-50 border border-gray-200 rounded-xl mb-4 p-4 flex-row gap-4">
             <Image
               source={pet.imagem}
               className="w-36 h-36 rounded-xl"
               resizeMode="cover"
             />
+
             <View className="flex-1 gap-2">
-              <View className="flex-row items-center gap-2">
-                <Text className="text-lg font-bold text-black">{pet.nome}</Text>
-                <Text className="text-gray-500">• {pet.idade}</Text>
+              <View className="flex-row justify-between items-center">
+                <View className="flex-row items-center gap-2">
+                  <Text className="text-lg font-bold text-black">
+                    {pet.nome}
+                  </Text>
+                  <Text className="text-gray-500">• {pet.idade}</Text>
+                </View>
+                <TouchableOpacity onPress={() => toggleFavorito(pet.id)}>
+                  <Ionicons name="heart" size={24} color={colors.green} />
+                </TouchableOpacity>
               </View>
 
-              <View className="flex-row flex-wrap gap-2 items-center">
-                {pet.filtros.slice(0, 2).map((filtro, index) => (
+              <View className="flex-row flex-wrap gap-2 overflow-hidden">
+                {pet.filtros.map((filtro, index) => (
                   <View
                     key={index}
                     className="bg-gray-100 border border-gray-200 px-2 py-2 rounded-full"
@@ -70,10 +88,6 @@ export default function FavoritesScreen() {
                 <Text className="text-gray-500 text-sm">{pet.local}</Text>
               </View>
             </View>
-
-            <TouchableOpacity>
-              <Ionicons name="heart" size={28} color={colors.green} />
-            </TouchableOpacity>
           </View>
         )}
       />
