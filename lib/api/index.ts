29985c188ff4e86d4272741e33a8ee8dc308bd1d
Ingrid Beatriz <ingridbeatriz_1@outlook.com>
2025-06-01 22:19:23 -0300

    feat: create authorization interceptor

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,5 +1,19 @@
 // lib/api/index.ts
 
+export type Interceptor = {
+  onRequest: (options: RequestInit) => Promise<RequestInit> | undefined;
+  onResponse: (
+    response: Response,
+    retryCB: () => Promise<any>
+  ) => Promise<any> | undefined;
+};
+
+type ApiCallOptions = {
+  endpoint: string;
+  interceptors: Interceptor[];
+  options: RequestInit;
+};
+
 const API_BASE_URL =
   process.env.EXPO_PUBLIC_API_URL || "http://192.168.0.104:3000/api";
 
@@ -18,4 +32,34 @@ export async function apiFetch(path: string, options: RequestInit = {}) {
   }
 
   return response.json();
+}
+
+export async function makeApiCall(options: ApiCallOptions): Promise<any> {
+  const initialOptions = { ...options.options };
+
+  for (const interceptor of options.interceptors) {
+    if (typeof interceptor.onRequest !== "function") {
+      continue;
+    }
+
+    options.options = await interceptor.onRequest(options.options)!;
+  }
+
+  var response = await apiFetch(options.endpoint, options.options);
+
+  for (const interceptor of options.interceptors.reverse()) {
+    if (typeof interceptor.onResponse !== "function") {
+      continue;
+    }
+
+    response = await interceptor.onResponse(response, () =>
+      makeApiCall({
+        endpoint: options.endpoint,
+        interceptors: options.interceptors,
+        options: initialOptions,
+      })
+    )!;
+  }
+
+  return response;
 }
